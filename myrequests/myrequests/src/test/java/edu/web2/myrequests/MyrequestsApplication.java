5c748c848f4fe5a1b0f00955021980cbe87b1b40
Julia Evangelista <109988092+juliaevangelista@users.noreply.github.com>
2023-08-16 10:54:17 -0300

    Add files via upload

---
+++ myrequests/myrequests/src/test/java/edu/web2/myrequests/MyrequestsApplication.java
@@ -0,0 +1,14 @@
+package edu.web2.myrequests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyrequestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyrequestsApplication.class, args);
+		
+	}
+
+}
